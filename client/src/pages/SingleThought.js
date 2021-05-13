1daac4b73781dd08ff72d8bee2b2529fdffbf0de
Your NameAlex <alehr45@gmail.com>
2021-05-13 17:01:26 -0500

    added reaction and thought components

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
+import ReactionForm from '../components/ReactionForm';
 import ReactionList from '../components/ReactionList';
-
+import Auth from "../utils/auth";
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
 
@@ -32,8 +32,8 @@ const SingleThought = props => {
           <p>{thought.thoughtText}</p>
         </div>
       </div>
-
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
